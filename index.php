caf0bb2c892116d9bef7f42c25b36807927d713f
issam-amin <issamamine@180gmail.com>
2024-04-05 02:50:37 +0000

    First commit

---
+++ index.php
@@ -0,0 +1,28 @@
+²<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>login page</title>
+</head>
+<body>
+    <div class="container">
+        <div  class="login">
+                <form action="login.php" method="post">
+                    <h2>Login</h2>
+                    <div class="input-group">
+                        <label for="username">Username :</label>
+                        <input type="text" name="username" id="username" required>
+                    </div>
+                    <div class="input-group">
+                        <label for="password">Password :</label>
+                        <input type="password" name="password" id="password" required>
+                    </div>
+                    <button type="submit" class="btn">Login</button>
+                </form> 
+        </div>
+       <img src="" alt="">
+    </div>
+</body>
+</html>
