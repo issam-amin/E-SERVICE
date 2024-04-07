e81bc46e08f4c08f833a5f12672c3729f688c89e
issam-amin <issamamine@gmail.com>
2024-04-07 15:12:06 +0000

    new login page

--- index.php
+++ index.php
@@ -10,22 +10,34 @@ include('database.php');
     <title>login page</title>
 </head>
 <body>
-    <div class="container">
-        <div  class="login">
-                <form action="login.php" method="post">
-                    <h2>Login</h2>
-                    <div class="input-group">
-                        <label for="username">Username :</label>
-                        <input type="text" name="username" id="username" required>
-                    </div>
-                    <div class="input-group">
-                        <label for="password">Password :</label>
-                        <input type="password" name="password" id="password" required>
-                    </div>
-                    <button type="submit" class="btn">Login</button>
-                </form> 
+    <!-- FLEX -->
+    <div class="container">    
+             <!-- login   -->
+        <form action="" method="post" >
+        <h2>LOGIN</h2>
+        <div class="input_id">
+            <input type="text" name="Email"  placeholder="E-mail" required>
+                        <i class="fa-solid fa-user"></i>
         </div>
-       <img src="" alt="">
+        <div class="input_id">
+            <input type="password" name="name" id="Password" placeholder="Password"  required>
+            <i class="fa-solid fa-lock"></i>
+        </div>
+    <!-- checkBox -->
+    <div class="remenber">
+        <label><input type="checkbox">Remenber me</label>
+            <a href="">forget password</a>
     </div>
+    
+    <button type="button" class="btn"> login </button>
+    
+    <div class="notregister">
+        <p>You Don't have an account ?<a href=""> Register</a></p>
+        
+    </div>
+    </form>
+    
+        
+
 </body>
 </html>
